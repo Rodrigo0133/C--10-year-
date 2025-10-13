e0e41a9814271f7bf76635d03a182e6b4e565d4d
Rodrigo Galante <rodrigo.galante@sapo.pt>
2025-10-13 12:42:46 +0100

    Add files via upload

---
+++ C++/disciplinas.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <locale.h>
+#include <string>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Portuguese");
+
+	int r;
+
+	string carros[5] = { "Português", "Matematica", "Inglês", "TLP","TIC"};
+
+	char Sair = 'n';
+	do {
+		cout << "Escolhe um número da materia (0 a 3) \n";
+		cin >> r;
+		if (r < 0 || r>4) {
+			cout << "Esse número não existe, seu idiota! \n";
+		}
+		else {
+			cout << "\n O número da materia é " << r << " e a materia é " << carros[r] << " .\n";
+		}
+		cout << "Deseja sair? (n/s) \n";
+		cin >> Sair;
+	} while (Sair != 's');
+	return 0;
+}
