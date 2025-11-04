ae6a788f408a883f2bb6ef08491528c7ab23d557
Rodrigo Galante <rodrigo.galante@sapo.pt>
2025-11-04 14:08:33 +0000

    Add files via upload

---
+++ C++/Mooshak/DesafioB.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <locale.h>
+#include <cstdlib>
+#include <time.h>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Portuguese");
+	int A, L, C, V;
+	cin >> A;
+	cin >> L;
+	cin >> C;
+	V = C * L * A;
+	if (A >= 3 && V >= 50)
+	{
+		cout << "1" << endl;
+	}
+	else 
+	{
+		cout << "0" << endl;
+	}
+
+		return 0;
+}
