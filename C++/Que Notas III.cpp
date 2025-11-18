597f26f3333e1ff19a22405a1976df1839e8b733
Rodrigo Galante <rodrigo.galante@sapo.pt>
2025-11-18 14:34:05 +0000

    Update Que Notas III.cpp

--- C++/Que Notas III.cpp
+++ C++/Que Notas III.cpp
@@ -4,34 +4,36 @@
 #include <cstdlib>
 #include <time.h>
 using namespace std;
- 
+
 int main()
 {
-	setlocale(LC_ALL, "Portuguese");
-	srand(time(0));
-	int i,cont=0,j;
-	float media[10], nota[10],Maior[10];
-	string nomes[10] = { "António","Anabela","Beatriz","Bernardo","Clara","Carlos","Diana","Diogo","Elisabete","Eurico" };
+    setlocale(LC_ALL, "Portuguese");
+    srand(time(0));
+    int i, cont = 0, j;
+    float media[10], nota[10], Maior[10];
+    string nomes[10] = { "António","Anabela","Beatriz","Bernardo","Clara","Carlos","Diana","Diogo","Elisabete","Eurico" };
 
-string disciplinas[10] = {"Portugues","Ingles","Fisica","TLP","TIC","Matemática","ACS","Filosofia","Quimica","Ed.Fis." };
-    for(i=0;i<10;i++){
-        Maior[i]= 0;
-         for( j = 0;j<10;j++){
-            cout << "Escreva a nota do/a "<< nomes[j] << " a " << disciplinas[i] << "\n"; 
-            cin>>nota[j];
-            if(Maior[i]<nota[j]){
-                Maior[i]=nota[j];
+    string disciplinas[10] = { "Portugues","Ingles","Fisica","TLP","TIC","Matemática","ACS","Filosofia","Quimica","Ed.Fis." };
+    for (i = 0; i < 10; i++) {
+        Maior[i] = 0;
+        media[i] = 0;
+        for (j = 0; j < 10; j++) {
+            cout << "Escreva a nota do/a " << nomes[j] << " a " << disciplinas[i] << "\n";
+            cin >> nota[j];
+            if (Maior[i] < nota[j]) {
+                Maior[i] = nota[j];
             }
-            
             media[i] = nota[j] + media[i];
-         }
-         media[i] = media[i]/3;
+        }
+        
     }
-for(i=0;i<10;i++){
-         for( j = 0;j<10;j++){
-             if(Maior[i] == nota[j]){
-                 cout << "A media de "<< disciplinas[i] << " é "<< media[i] << ", A maior nota foi "<< Maior[i]<< " do/a aluno/a " << nomes[j];
-             }
-         }}
-	return 0;
+    for (i = 0; i < 10; i++) {
+        media[i] = media[i] / 10;
+        for (j = 0; j < 10; j++) {
+            if (Maior[i] == nota[j]) {
+                cout << "A media de " << disciplinas[i] << " é " << media[i] << ", A maior nota foi " << Maior[i] << " do/a aluno/a " << nomes[j] << "\n";
+            }
+        }
+    }
+    return 0;
 }
