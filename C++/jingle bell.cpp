0752a8bf60b03c56acce3fbdbbd5222a621980e1
Rodrigo Galante <rodrigo.galante@sapo.pt>
2025-12-18 16:36:21 +0000

    Update jingle bell.cpp

--- C++/jingle bell.cpp
+++ C++/jingle bell.cpp
@@ -1,64 +1,34 @@
-#include <iostream> 
-#include <cstring> 
+#include <iostream>
+#include <cstdlib>
+#include <time.h>
+#include <locale.h>
 #include <string>
-#include <locale> 
-#include <cctype>   
 using namespace std;
+ 
 int main() {
-    setlocale(LC_ALL, "Portuguese");
-    char NomeP[10000];
-    int i = -1, po;
-    char Maisculas[26] = { 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z' }, minusculas[26] = { 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' };
-    do {
-        i++;
-        cin >> NomeP[i];
-    } while (NomeP[i] != '.');
-    for (int u = 0; u < i; u++) {
-        for (int l = 0; l < 26; l++) {
-            if (NomeP[u] == Maisculas[l]) {
-                cout << NomeP[u];
-                u += 1;
-                break;
-            }
-        }
-        do {
-            po = 0;
-            for (int j = 0; j < 26; j++) {
+	setlocale(LC_ALL, "Portuguese");
+	int total[3]= {0}, quantidades[3][3] = {0}, total2[3] = {0}, totalfinal = 0;
+	string nomes[3] = { "Rodrigo","Manuel","Joaquim"}, prendas[3] = { "Iphone","Pc","Ps5" };
+	for (int i = 0; i < 3; i++) {
+		for (int ii = 0; ii < 3; ii++) {
+			cout << " quantos " << prendas[ii] << " quer para o " << nomes[i];
+			cin >> quantidades[i][ii];
+			total[i] += quantidades[i][ii];
+			total2[i] += quantidades[ii][i];
+		}
+	}
+	for (int i = 0; i < 3; i++) {
+		for (int ii = 0; ii < 3; ii++) {
+			cout << "O " <<nomes[i]<<" teve o total de "<< total[ii]<< " de "<< prendas[i]<<"" << endl;
+			cout << "O produto " << prendas[ii] << " teve o total de " << total2[i] << "\n";
 
-                if (NomeP[u] == minusculas[j]) {
-                    cout << NomeP[u];
-                    u += 1;
-                    po++;
-                    break;
-                }
-            }
-        } while (po != 0);
-        if (isdigit(NomeP[u])) {
-            cout << "\t" << NomeP[u];
-            u += 1;
-            while (isdigit(NomeP[u])) {
-                if (isdigit(NomeP[u])) {
-                    cout << NomeP[u];
-                    u += 1;
-                }
-            }
-        }
-        else {
-            cout << "\t 1";
-        }
-        for (int y = 0; y < 26; y++) {
-            if (NomeP[u] == Maisculas[y]) {
-                cout << "\n";
-
-                break;
-            }
-        }
-        u -= 1;
-
-
-
-
-    }
-    return 0;
+ 
+			total[i] += quantidades[i][ii];
+		} 
+	}  
+	for (int i = 0; i < 3; i++) {
+		totalfinal = total[i] + total2[i];
+	}
+	cout << "o total de tudo foi " <<totalfinal;
+	return 0;
 }
-
